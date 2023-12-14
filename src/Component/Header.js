038e710d3b61fb78851e810e0a63667de818635b
Pavi21 <82439522+Pavithiran21@users.noreply.github.com>
2023-12-15 00:07:14 +0530

    Add files via upload

---
+++ src/Component/Header.js
@@ -0,0 +1,48 @@
+// import React from 'react'
+// import { Container, Nav, Navbar} from 'react-bootstrap'
+
+// export const Header = () => {
+//   return (
+//     <Navbar expand="lg"  sticky='top' className='navbar bg-success' >
+//     <Container fluid>
+//       <Navbar.Brand href="#"><span className='blue-text'>Pavithiran</span></Navbar.Brand>
+//       <Navbar.Toggle aria-controls="basic-navbar-nav"  />
+//       <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+//         <Nav className="ms-auto ">
+//           <Nav.Link href="#about">ABOUT</Nav.Link>
+//           <Nav.Link href="#skill">SKILLS</Nav.Link>
+//           <Nav.Link href="#project">PROJECTS</Nav.Link>
+//           <Nav.Link href="#contact">CONTACT</Nav.Link>
+        
+//         </Nav>
+//       </Navbar.Collapse>
+//     </Container>
+//   </Navbar>
+  
+//   )
+// }
+
+
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+export const Header = () => {
+  return (
+    <Navbar expand="lg" sticky="top">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <span>Pavithiran</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="ms-auto">
+            <Nav.Link href="#about" className='text-white'>ABOUT</Nav.Link>
+            <Nav.Link href="#skill" className='text-white' >SKILLS</Nav.Link>
+            <Nav.Link href="#project" className='text-white'>PROJECTS</Nav.Link>
+            <Nav.Link href="#contact"className='text-white'>CONTACT</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
